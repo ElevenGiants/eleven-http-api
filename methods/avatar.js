e4328ef68090432f3131d50a65b850528e8232dc
Joey Thomas <josephthomas619@msn.com>
2014-12-07 15:49:00 +0000

    Stub avatar.getHistory
    Used in changing outfits, which currently does not work. Returning an
    empty history tells the user they have no outfits.

---
+++ methods/avatar.js
@@ -0,0 +1,11 @@
+var utils = require('../utils');
+
+
+/*
+ * avatar.getHistory
+ * Fetch a players saved customizations.
+ */
+exports.getHistory = function(req, pc) {
+	// TODO: Implement me.
+	return { history: {}, total: 0 };
+};
