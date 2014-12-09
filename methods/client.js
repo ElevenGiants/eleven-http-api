3df04f4c3a62136e361e49c70fb87b31ab372214
Joey Thomas <josephthomas619@msn.com>
2014-12-09 17:50:09 +0000

    Use config.reportDir when storing reports/images

--- methods/client.js
+++ methods/client.js
@@ -13,7 +13,7 @@ exports.attachErrorImage = function(req, pc) {
 		for (var file in req.files) {
 			if (req.files.hasOwnProperty(file)) {
 				var errorImage = fs.readFileSync(req.files[file].path);
-				fs.writeFileSync('reports/saved/' + req.body.error_id + '.png', errorImage);
+				var test = fs.writeFileSync(config.reportDir + '/' + req.body.error_id + '.png', errorImage);
 			}
 		}
 		return {};
@@ -32,7 +32,7 @@ exports.attachErrorImage = function(req, pc) {
 exports.error = function(req, pc) {
 	try {
 		var error_id = pc + '_' + Date.now();
-		fs.writeFileSync('reports/saved/' + error_id + '.json', JSON.stringify(req.body));
+		var test = fs.writeFileSync(config.reportDir + '/' + error_id + '.json', JSON.stringify(req.body));
 		sendMessageToSlack('New error report received: ' + error_id, [{
 			title: req.body.flash_error.split('\n')[0],
 			value: req.body.user_error + 
