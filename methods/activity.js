4a903fa467fb57a1c82756cf5618ae948d7fb113
Joey Thomas <josephthomas619@msn.com>
2014-12-09 16:10:19 +0000

    Stub activity related APIs
    This stops errors from being sent when the player opens the "Updates"
    window.

---
+++ methods/activity.js
@@ -0,0 +1,22 @@
+var utils = require('../utils');
+
+
+/*
+ * activity.feed
+ * Returns the players activity feed.
+ */
+exports.feed = function(req, pc) {
+	// TODO: Implement me.
+	return {};
+};
+
+
+/*
+ * activity.setStatus
+ * Sets the players activity status.
+ */
+exports.setStatus = function(req, pc) {
+	// TODO: Implement me.
+	return {};
+};
+
