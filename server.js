19c5e067097980a937e526898d20c0afa7c71718
Justin Patrin <papercrane@reversefold.com>
2015-02-02 22:51:10 -0800

    Add CORS headers so we can call the http-api from javascript in the webapp

--- server.js
+++ server.js
@@ -21,6 +21,11 @@ function init() {
 		uploadDir: config.tmpDir,
 		maxFilesSize: '10mb'
 	});
+	app.use(function(req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		next();
+	});
 	app.use(bodyParser.urlencoded({
 		extended: true,
 		uploadDir: config.tmpDir,
