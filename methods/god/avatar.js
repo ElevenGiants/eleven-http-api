13c4467e60c9bbe7870bc7c1963e63dc997fc6f7
Markus Dolic <crowley@gmx.net>
2016-05-15 21:34:31 +0200

    Configurable spritesheet/avatar assets directory
    * make the spritesheet/singles storage directory configurable (can
      point to a local directory served to the client if desired)
    * store avatar assets with path/file name structure similar to Glitch
      (date based path, unique file name with timestamp; just using the
      player TSID instead of a hash)
    * add some logging
    * minor unrelated cosmetic changes in `server.js` to fix lint warnings

--- methods/god/avatar.js
+++ methods/god/avatar.js
@@ -1,3 +1,4 @@
+var config = require('config');
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 var wait = require('wait.for');
@@ -7,6 +8,7 @@ var wait = require('wait.for');
  * god.avatar.saveSpritesheets
  */
 exports.saveSpritesheets = function saveSpritesheets(req, pc) {
+	log.info('saving spritesheets for %s', pc);
 	return saveAvatarImages(req, pc, 'sheets', 'avatar_set_sheets');
 };
 
@@ -15,21 +17,34 @@ exports.saveSpritesheets = function saveSpritesheets(req, pc) {
  * god.avatar.saveSingles
  */
 exports.saveSingles = function saveSingles(req, pc) {
+	// TODO: this only includes the left-faced singles; it seems we need to
+	// create&save the flipped versions ourselves (unless we don't need them?
+	// e.g., the client seems to flip the image itself in player info dialog)
+	log.info('saving singles for %s', pc);
 	return saveAvatarImages(req, pc, 'singles', 'avatar_set_singles');
 };
 
 
 function saveAvatarImages(req, pc, type, apiFunc) {
-	var urlPath = '/c2.glitch.bz/avatars/' + pc + '/' + type + '/';
-	var dir = __dirname + '/../eleven-assets' + urlPath;
+	var dateStr = new Date().toISOString().split('T')[0];
+	var timeStr = '' + Math.floor(new Date().getTime() / 1000);
+	var prefix = pc + '_' + timeStr;
+	var urlPath = config.assetPaths.avatars + '/' + dateStr;
+	var dir = config.assetDir + urlPath;
+	log.info('writing images for %s to %s', pc, dir);
 	wait.for(mkdirp, dir);
 	for (var key in req.files) {
 		var file = req.files[key];
-		//TODO: Store in outfit directories? Store with a timestamp/date/random number like TS did?
-		data = wait.forMethod(fs, 'readFile', file.path);
-		wait.forMethod(fs, 'writeFile', dir + file.name + '.png', data);
+		var name = file.name.replace(/^image_?/, '');
+		if (!name.length) name = '172';  // special case for full-size single
+		var target = dir + '/' + prefix + '_' + name + '.png';
+		log.debug('writing %s/%s to %s', pc, key, target);
+		// copying instead of moving here because the multipart middleware wants
+		// to delete the temp files afterwards
+		var data = wait.forMethod(fs, 'readFile', file.path);
+		wait.forMethod(fs, 'writeFile', target, data);
 	}
 	// save spritesheet paths to GS
-	rpcObjCall(pc, apiFunc, [{url: urlPath + 'image'}]);
+	rpcObjCall(pc, apiFunc, [{url: urlPath + '/' + prefix}]);
 	return {};
 }
