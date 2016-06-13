259ced0bede73362a6a3d1763de4b25b2e3c73e9
Joey Thomas <joey.thomas@outlook.com>
2016-06-13 17:27:18 +0100

    Update api methods
    * players.fullInfo now returns the players spritesheets and pol info
    * snaps.randomForCamera now returns GG by default until snaps work

--- methods/players.js
+++ methods/players.js
@@ -8,17 +8,24 @@ exports.fullInfo = function fullInfo(req, pc) {
 	}
 	var body = rpcObjCall(req.query.player_tsid, 'adminGetFullInfo',
 		[{viewer_tsid: pc}]);
-	var player = rpcCall('api', 'apiFindObject', [req.query.player_tsid]);
+	var player = rpcCall('api', 'apiGetObjectContent', [req.query.player_tsid]);
 	body.player_name = player.label;
 	body.player_tsid = req.query.player_tsid;
-	// TODO: Use the players spritesheets.
-	//jscs:disable maximumLineLength
 	body.avatar = {
-		172: '/c2.glitch.bz/avatars/2011-06-03/2765262852ce6775fa7a497259aecb39_1307145346_172.png',
-		100: '/c2.glitch.bz/avatars/2011-06-03/2765262852ce6775fa7a497259aecb39_1307145346_100.png',
-		50: '/c2.glitch.bz/avatars/2011-06-03/2765262852ce6775fa7a497259aecb39_1307145346_50.png',
+		172: player.av_meta.singles + '_172.png',
+		100: player.av_meta.singles + '_100.png',
+		50: player.av_meta.singles + '_50.png',
 	};
-	//jscs:enable maximumLineLength
+	body.relationship = {
+		is_blocking_me: !body.can_contact,
+		can_contact: body.can_contact
+	};
+	if (body.houses) {
+		body.pol = {
+			tsid: body.houses.street.tsid,
+			street_tsid: body.houses.street.tsid
+		};
+	}
 	// TODO: Use the players status.
 	body.last_update = {
 		id: 0,
